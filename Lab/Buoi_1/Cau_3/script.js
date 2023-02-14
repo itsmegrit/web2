7fa10a0e19feb37629f29f6a63b126821d0620a0
Phong <ngt.phongg@gmail.com>
2023-02-14 07:22:11 +0700

    update script.js

--- Lab/Buoi_1/Cau_3/script.js
+++ Lab/Buoi_1/Cau_3/script.js
@@ -1,42 +1,67 @@
-function chkfN(input) {
-  var name = document.getElementById(input);
-
-  var pos = name.value;
-  if (pos == "") {
-    name.style.borderColor = "red";
-    return false;
-  } else {
-    name.style.borderColor = "";
-    return true;
-  }
-}
-
-function chkFocus(input) {
-  var name = document.getElementById(input);
-
-  var pos = name.value;
-  if (pos == "") {
-    name.focus();
-    return false;
-  } else {
-    name.style.borderColor = "";
-    return true;
-  }
-}
-
 const submitBtn = document.getElementById("submitBtn");
-submitBtn.addEventListener("click", function () {
-  chkfN("nameInputID");
-  chkfN("addressInputID");
-  chkfN("PIDInputID");
 
-  if (!chkFocus("nameInputID")) {
+var nameInput = document.getElementById("nameInputID");
+var addressInput = document.getElementById("addressInputID");
+var PIDInput = document.getElementById("PIDInputID");
+
+function chkRegex(obj, regex) {}
+
+function chkFocusOut(obj) {
+  if (obj.value == "") {
+    obj.style.borderColor = "red";
+    return false;
+  } else {
+    obj.style.borderColor = "";
+    return true;
+  }
+}
+
+nameInput.addEventListener("focusout", function () {
+  chkFocusOut(nameInput);
+});
+
+addressInput.addEventListener("focusout", function () {
+  chkFocusOut(addressInput);
+});
+
+PIDInput.addEventListener("focusout", function () {
+  chkFocusOut(PIDInput);
+});
+
+function chkfN(obj) {
+  var pos = obj.value;
+  if (pos == "") {
+    obj.style.borderColor = "red";
+    return false;
+  } else {
+    obj.style.borderColor = "";
+    return true;
+  }
+}
+
+function chkFocus(obj) {
+  var pos = obj.value;
+  if (pos == "") {
+    obj.focus();
+    return false;
+  } else {
+    obj.style.borderColor = "";
+    return true;
+  }
+}
+
+submitBtn.addEventListener("click", function () {
+  chkfN(nameInput);
+  chkfN(addressInput);
+  chkfN(PIDInput);
+
+  if (!chkFocus(nameInput)) {
     return false;
   }
-  if (!chkFocus("addressInputID")) {
+  if (!chkFocus(addressInput)) {
     return false;
   }
-  if (!chkFocus("PIDInputID")) {
+  if (!chkFocus(PIDInput)) {
     return false;
   }
 });
