b4b041c6953e676862ced83e76af32f356bd836e
Phong <ngt.phongg@gmail.com>
2023-03-21 16:29:34 +0700

    init home page style

---
+++ ToyKinhDom/public_html/header.php
@@ -0,0 +1,13 @@
+<!-- Header -->
+
+<div class="layoutWidth">
+  <div class="headerLeft">
+    <div class="headerLogo">Logo here</div>
+    <div class="findProduct"></div>
+  </div>
+
+  <div class="headerRight">
+    <div class="headerRightButton">Login/Logout</div>
+    <div class="headerRightButton">Cart</div>
+  </div>
+</div>
