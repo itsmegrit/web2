796b840ded8ef96d517e72db5dda32e51548b16d
Phong <ngt.phongg@gmail.com>
2023-03-28 16:40:36 +0700

    update header

--- ToyKinhDom/index1.php
+++ ToyKinhDom/index1.php
@@ -6,13 +6,14 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>TOY KINHDOM</title>
-  <link rel="stylesheet" href="./includes/commonStyle.css">
-  <link rel="stylesheet" href="./includes/bootstrap-5.3.0-alpha1/bootstrap-5.3.0-alpha1/dist/css/bootstrap.min.css">
-  <link rel="stylesheet" href="./includes/bootstrap-5.3.0-alpha1/bootstrap-5.3.0-alpha1/dist/css/bootstrap-grid.min.css">
-  <link rel="stylesheet" href="./includes/bootstrap-5.3.0-alpha1/bootstrap-5.3.0-alpha1/dist/css/bootstrap-utilities.min.css">
-  <link rel="stylesheet" href="./includes/bootstrap-5.3.0-alpha1/bootstrap-5.3.0-alpha1/dist/css/bootstrap-reboot.min.css">
-  <link rel="stylesheet" href="./includes/bootstrap-5.3.0-alpha1/bootstrap-5.3.0-alpha1/dist/css/bootstrap.rtl.min.css">
+  <link rel="stylesheet" href="./includes/homePage.css">
+  <link rel="stylesheet" href="./includes/bootstrap-5.3.0-alpha1/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="./includes/bootstrap-5.3.0-alpha1/dist/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="./includes/bootstrap-5.3.0-alpha1/dist/css/bootstrap-utilities.min.css">
+  <link rel="stylesheet" href="./includes/bootstrap-5.3.0-alpha1/dist/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="./includes/bootstrap-5.3.0-alpha1/dist/css/bootstrap.rtl.min.css">
   <link rel="stylesheet" href="./public_html/publicHeader/headerStyle.css">
+  <link rel="stylesheet" href="./public_html/publicHeader/headerStyle.scss">
   <link rel="stylesheet" href="./public_html/publicBody/bodyStyle.css">
   <link rel="stylesheet" href="./public_html/publicFooter/footerStyle.css">
 </head>
