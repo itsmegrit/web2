14d4df982206eac69bb870845ef44d443a976311
Phong <ngt.phongg@gmail.com>
2023-03-28 13:32:47 +0700

    update footer

--- ToyKinhDom/includes/Footer/footer.php
+++ ToyKinhDom/includes/Footer/footer.php
@@ -1,3 +1,56 @@
-<div class="commonLayout footer">
-  Footer
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-sm-6">
+      <h6>ĐĂNG KÍ NHẬN THÔNG TIN KHUYẾN MÃI</h6>
+      <div class="input-group">
+        <input type="text" class="form-control" placeholder="Nhập email">
+        <button class="btn btn-primary " id="btn-email">
+          <img src="img/icons8-secured-letter-50.png" class="img-rounded" alt="Cinque Terre" id="email">
+        </button>
+      </div>
+    </div>
+    <div class="col-sm-6">
+      <h6>THEO DÕI</h6>
+      <div class="social">
+        <a href="#"><img src="img/facebook.png" class="img-rounded" alt="Cinque Terre"></a>
+        <a href="#"><img src="img/instagram.png" class="img-rounded" alt="Cinque Terre"></a>
+        <a href="#"> <img src="img/youtube.png" class="img-circle" alt="Cinque Terre"></a>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-4 footerform">
+      <h5>HOTLINE CHĂM SÓC KHÁCH HÀNG</h5>
+      <h3>19001208</h3>
+      <p>Từ thứ Hai đến thứ Bảy (08:00 - 17:00)
+      </p>
+      <p>Chủ nhật (08:00 - 12:00)</p>
+      <p> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+          <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
+        </svg><a href="">hotro@toykingdom.com.vn</a>
+      </p>
+    </div>
+    <div class="col-sm-4">
+      <h5>HOTLINE CHĂM SÓC KHÁCH HÀNG</h5>
+      <p><a href="#">- Chính sách giao hàng</a></p>
+      <p><a href="#">- Chính sách tích lũy điểm</a></p>
+      <p><a href="#">- Điều khoản điều kiện</a></p>
+    </div>
+    <div class="col-sm-4">
+      <h5>HỖ TRỢ KHÁCH HÀNG</h5>
+      <p><a href="#">- Chính sách bảo mật</a></p>
+      <p><a href="#">- Chính sách bảo hành đổi trả hàng hóa</a></p>
+      <p><a href="#">- Chính sách thanh toán</a></p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-15">
+      <img class="img-responsive" src="img/logoSaleNoti.png" alt="Chania" id="">
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-15">
+      <p class="footer-reserved">© All rights reserved.</p>
+    </div>
+  </div>
 </div>
