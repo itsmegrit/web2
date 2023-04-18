52d45b5468295f8a929e741e5babd80f729f80f5
Phong <ngt.phongg@gmail.com>
2023-04-18 16:06:50 +0700

    update header

--- ToyKinhDom/public_html/publicHeader/header.php
+++ ToyKinhDom/public_html/publicHeader/header.php
@@ -4,24 +4,26 @@
       <div class="col-sm-2">
         <img class="img-responsive" src="./public_html/img/logo.png" alt="Chania" id="">
       </div>
-      <div class="input-group col-sm-5">
-        <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
-        <button type="button" class="btn btn-outline-primary">
-          <i class="fas fa-search"></i>
-        </button>
+      <div class="col-sm-5 d-flex align-items-center">
+        <div class="input-group header-h-32px">
+          <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+          <button type="button" class="btn btn-outline-primary">
+            <i class="fas fa-search"></i>
+          </button>
+        </div>
       </div>
       <div class="col-sm-5">
         <div class="row d-flex align-items-center h-100">
           <div class="col-sm-4 d-flex justify-content-end align-items-center">
-            <i class="fa-solid fa-receipt"></i>
-            Tra cứu đơn hàng
+            <i class="fa-solid fa-receipt header-mr-8px"></i>
+            Đơn hàng
           </div>
           <div class="col-sm-4 d-flex justify-content-end align-items-center">
-            <i class="fas fa-shopping-cart"></i>
+            <i class="fas fa-shopping-cart header-mr-8px"></i>
             Giỏ hàng
           </div>
           <div class="col-sm-4 d-flex justify-content-end align-items-center">
-            <i class="fa-regular fa-user"></i>
+            <i class="fa-regular fa-user header-mr-8px  "></i>
             Tài khoản
           </div>
         </div>
