d54c62eba8ac72dac32586061b07612761c21fc0
Phong <ngt.phongg@gmail.com>
2023-05-14 07:23:50 +0700

    update connection

--- ToyKinhDom/config/Connection.php
+++ ToyKinhDom/config/Connection.php
@@ -4,9 +4,9 @@ class Connect
   public $conn;
   function __construct()
   {
-    $servername = "localhost:3307";
-    $username = "QLBDC";
-    $password = "123456";
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
     $dbname = "toykingdom";
     $this->conn = new mysqli($servername, $username, $password, $dbname);
     if ($this->conn->connect_error) {
@@ -19,7 +19,7 @@ class Connect
   }
   function selectsql($table, $colum = "*", $condition = null)
   {
-    $sql = "SELECT $colum from $table $condition";
+    $sql = "SELECT $colum from $table $condition ";
     $result = $this->conn->query($sql);
     return $result;
   }
